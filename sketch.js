f3d708be33684fd2eab517f0058b639042e71408
JaanviM0602 <88243794+JaanviM0602@users.noreply.github.com>
2021-08-17 16:26:16 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+var rock;
+var wall;
+var wedge;
+var stand1;
+var stand2;
+var ground;
+var angle = 1;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 1.5,
+    frictionAir:0.02
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  var rock_options ={
+    restitution: 0.85,
+  
+  }
+  var wedge_options = {
+    isStatic: true,
+  }
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  rock = Bodies.circle(300,20,10,rock_options);
+  World.add(world,rock);
+  wall = Bodies.rectangle(300,200,200,20,ground_options);
+  World.add(world,wall);
+  wedge = Bodies.rectangle(100,200,100,20,wedge_options);
+  World.add(world,wedge);
+
+  console.log(ball);
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+  
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,400,20);
+  ellipse(rock.position.x,rock.position.y,20);
+  rect(wall.position.x,wall.position.y,200,10);
+  Body.rotate(wedge,angle);
+  translate(wedge.position.x,wedge.position.y);
+  rotate(angle);
+  rect(0,0,100,20);
+  angle += 0.1;
+}
+
